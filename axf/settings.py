65e8a1ad1b63dfdaaff988fda009dc7a389ad1e0
liuda <1625211623@qq.com>
2019-03-08 10:56:42 +0800

    mysql pwd

--- axf/settings.py
+++ axf/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'HOST':"127.0.0.1",
         'PORT':3306,
         'USER': "root",
-        'PASSWORD': os.environ.get("MYSQL_PWD")
+        'PASSWORD': "liuda6015?"
     }
 }
 
