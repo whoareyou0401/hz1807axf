2c1cc16808fa17f8bca7b2b1b1a55531159b45e6
liuda <1625211623@qq.com>
2019-03-08 11:00:24 +0800

    debug false

--- axf/settings.py
+++ axf/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '82k#rm!y0rlp_bbt%vupbln38px+@40exj6()w#8o2=xn(#w4c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "*"
